ddbd68f9b2f1fd64d96f33df4fb4c3dcd3575963
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-01-05 12:27:03 -0500

    asdfvbgfshb

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -2,8 +2,6 @@ const DOMSelectors = {
   search: document.getElementById("search"),
   searchEnter: document.getElementById("searchEnter"),
   searchClear: document.getElementById("searchClear"),
-  list: document.getElementById("operatorList"),
-  card: document.querySelectorAll(".operatorCard"),
 };
 
 export { DOMSelectors };
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -2,27 +2,29 @@ import "../styles/style.css";
 import "./api";
 import { DOMSelectors } from "./dom";
 
-DOMSelectors.searchEnter.addEventListener("click", function (event) {
+DOMSelectors.search.addEventListener("keyup", (event) => {
   event.preventDefault();
-  const input = DOMSelectors.search.value;
-  const filter = input.value.toUpperCase();
-  for (i = 0; i < card.length; i++) {
-    a = card[i].getElementsByTagName("li")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      card[i].style.display = "";
-    } else {
-      card[i].style.display = "none";
-    }
+  const value = event.target;
+  const string = value.toString();
+  const query = string.toLowerCase();
+  let name = document.querySelectorAll(".op-name");
+  let card = document.querySelectorAll(".operatorCard");
+  let nameString = name.toString();
+  if (nameString.includes(query)) {
+    card.style.display = "block";
+  } else {
+    card.style.display = "none";
   }
 });
+
+DOMSelectors.searchClear.addEventListener("click", () => {});
 
 const displayOperators = (operators) => {
   const htmlString = operators
     .map((operator) => {
       return `
             <li class="operatorCard">
-                <h2>${operator.name}</h2>
+                <h2 class="op-name">${operator.name}</h2>
                 <p>Class: ${operator.class}</p>
                 <img src="${operator.art.Base}" class="img"></img>
             </li>
